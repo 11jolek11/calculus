346d296d97869b289b9390d49f09a0255a47a61a
11jolek11 <dabrowskiandrzej094@gmail.com>
2022-11-27 00:48:21 +0100

    Elementary functions handling added. Generating random numbers from uniform distribution to increase accuracy

--- gui_v3.0.py
+++ gui_v3.0.py
@@ -7,7 +7,7 @@ import square
 import trap
 
 
-class gui():
+class gui:
     def __init__(self):
         root = Tk()
         root.title("Calculus")
@@ -22,13 +22,12 @@ class gui():
         choose_label.grid()
         radio_recint = Radiobutton(frame_choose, variable=v, value=0, text="Recint")
         radio_recint.grid()
-        radio_montecarlo = Radiobutton(frame_choose,variable=v, value=1, text="Monte Carlo")
+        radio_montecarlo = Radiobutton(frame_choose, variable=v, value=1, text="Monte Carlo")
         radio_montecarlo.grid()
-        radio_trap = Radiobutton(frame_choose,variable=v, value=2, text="Trap")
+        radio_trap = Radiobutton(frame_choose, variable=v, value=2, text="Trap")
         radio_trap.grid()
-        radio_square = Radiobutton(frame_choose,variable=v, value=3, text="Square")
+        radio_square = Radiobutton(frame_choose, variable=v, value=3, text="Square")
         radio_square.grid()
-
 
         # Frame "x" and entries inside init
 
@@ -47,59 +46,23 @@ class gui():
         xk_entry = Entry(frame_x)
         xk_entry.grid()
 
-
         # Frame "entry" and "n" entry inside init
 
         frame_entry = Frame(root)
         frame_entry.grid(row=0, column=1)
         entry_label = Label(frame_entry, text="Entry formula: ")
         entry_label.grid()
-        entry_formula = Entry(frame_entry).insert(END, "Remember! You need specify every single math operation!")
+        # entry_formula = Entry(frame_entry).insert(END, "Remember! You need specify every single math operation!")
+        entry_formula = Entry(frame_entry)
+        entry_formula.insert(END, "Remember! You need specify every single math operation!")
         entry_formula.grid()
         entry_n_label = Label(frame_entry, text="n=")
         entry_n_label.grid()
         entry_n = Entry(frame_entry)
         entry_n.insert(END, '1000')
         entry_n.grid()
-
-
-        # Test: radiobuttons:
-        # choose_label.grid()
-        # radio_recint.grid()
-        # radio_montecarlo.grid()
-        # radio_trap.grid()
-        # radio_square.grid()
-        # frame_choose.grid()
-
-        # Test: X Entries:
-
-        # x_label.grid()
-        # frame_x_title.grid()
-
-        # xp_label.grid(row = 1, column=0)
-        # xp_entry.grid(row = 1, column=1)
-
-        # xk_label.grid(row = 2, column=0)
-        # xk_entry.grid(row = 2, column=1)
-
-        # x_label.grid()
-        # frame_x_title.grid(row = 0)
-
-        # frame_x.grid()
-
-        # Test: n & formula entry:
-        # frame_entry.grid()
-
-
-        # entry_label.grid(row = 0)
-        # entry_formula.grid(row = 1)
-
-        # entry_n_label.grid()
-        # entry_n.grid()
-
-        # frame_entry.grid()
-
-
         mainloop()
 
-test_object = gui()
+
+if __name__ == "__main__":
+    test_object = gui()
