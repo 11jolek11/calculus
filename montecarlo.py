79da42040e99d0867156ea5d903e11b6d09d9aca
11jolek11 <dabrowskiandrzej094@gmail.com>
2022-11-28 01:39:20 +0100

    sync

--- montecarlo.py
+++ montecarlo.py
@@ -1,5 +1,3 @@
-#!/usr/bin python3
-# -*- coding: utf-8 -*-
 import random
 import math
 
@@ -7,47 +5,89 @@ import math
 class MonteCarlo:
     """
     Integrating using Monte Carlo method.
-    Supports some of elementary functions
+    Supports some of elementary functions.
+    Extended numerical integration avoiding 'h' finding problem
     """
-    xp = 0
-    xk = 0
-    dx = 0
-    i = 0
-    n = 0
-    formula = ""
-
-    def __init__(self, xp, xk, formula, n=100000):
+    def __init__(self, xp:float, xk:float, formula:str, n:int=1000000):
         self.formula = formula
         for _ in self.formula:
             self.formula.replace('\n', '')
         self.n = n
         self.xp = xp
         self.xk = xk
-        self.dx = (self.xk - self.xp)/n
+        # self.dx = (self.xk - self.xp)/n
+        self.dx = self.xk - self.xp
 
     def fx(self):
         """
         Function fx represents function f(x) provided by user
         """
         return eval("lambda x:" + self.formula, math.__dict__)
-        # return lambda x: eval(self.formula)
 
     def start(self):
         """
         Starts calculating value.
         """
         s = 0
+        # i = 0
+        func = self.fx()
         for i in range(self.n):
             if i <= self.n-2:
                 temp = s
                 r = random.uniform(self.xp, self.xk)
-                func = self.fx()
                 s = temp + func(r)
             else:
-                s = (s/self.n)
+                s = (s/self.n)*self.dx
         return s
+
+class MonteCarlo2:
+    """
+    Integrating using Monte Carlo method.
+    Supports some of elementary functions.
+    """
+    # FIXME: nie radzi sobie z xp < 0
+    # FIXME: nie działa w 100%
+    def __init__(self, xp:float, xk:float, g:float, d:float, formula:str, n:int=1000000):
+        self.formula = formula
+        for _ in self.formula:
+            self.formula.replace('\n', '')
+        self.n = n
+        self.xp = xp
+        self.xk = xk
+        self.g = g
+        self.d = d
+        self.dx = (self.xk - self.xp)/n
+        self.inside = 0
+
+    def fx(self):
+        """
+        Function fx represents function f(x) provided by user
+        """
+        return eval("lambda x:" + self.formula, math.__dict__)
+
+    def start(self):
+        """
+        Starts calculating value.
+        """
+        func = self.fx()
+        s = 0
+        l_plus = 0
+        for i in range(self.n):
+            xi = random.uniform(self.xp, self.xk)
+            yi = random.uniform(self.d, self.g)
+            r = func(xi)
+            if r >= 0 and r >= yi:
+                l_plus += 1
+            elif r <= 0 and r < yi:
+                l_plus -=1
+            else:
+                l_plus += 0
+        return l_plus/self.n
+
 
 
 if __name__ == "__main__":
-    test_object = MonteCarlo(0, 1, "sin(x)")
+    test_object = MonteCarlo(0, math.pi, "sin(x)")
     print(test_object.start())
+    test = MonteCarlo2(0, math.pi, g=0, d=1, formula="sin(x)")
+    print(test.start())
